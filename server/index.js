f43f6bcb3e04c308949fef4e62f867c42812bfc8
Jose Gonzalez <otmaro@outlook.com>
2020-04-02 12:03:52 -0600

    Add swagger UI

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+
 
 const logger = require('./services/logger');
 const folderRoutes = require('./routes/folder');
@@ -6,10 +8,17 @@ const swaggerDocs = require('./routes/docs');
 const noteRoutes = require('./routes/note');
 const userRoutes = require('./routes/user');
 
+const swaggerUiOptions = {
+  swaggerOptions: {
+    url: 'http://localhost:3001/swagger.json',
+  },
+};
+
 const app = express();
 
 const port = 3001;
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, swaggerUiOptions));
 app.use('/', folderRoutes);
 app.use('/', swaggerDocs);
 app.use('/', noteRoutes);
