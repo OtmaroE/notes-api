93dbf8c69556b70b9d8da20daf61bfc51479e251
Jose Gonzalez <otmaro@outlook.com>
2020-04-02 09:29:10 -0600

    Refactor swagger docs out of server definition

--- server/index.js
+++ server/index.js
@@ -1,44 +1,17 @@
 const express = require('express');
-const swaggerJSDoc = require('swagger-jsdoc');
 
 const logger = require('./services/logger');
-const routes = require('./routes/folder');
+const folderRoutes = require('./routes/folder');
+const swaggerDocs = require('./routes/docs');
 
 const app = express();
 
-// TODO: Move swagger definition and endpoint out of index.js
-// swagger definition
-const swaggerDefinition = {
-  info: {
-    title: 'Notes API',
-    version: '1.0.0',
-    description: 'Simple note taking app',
-  },
-  host: 'localhost:3000',
-  basePath: '/',
-};
-
-// options for the swagger docs
-const options = {
-  // import swaggerDefinitions
-  swaggerDefinition,
-  // path to the API docs
-  apis: ['./server/routes/folder/*.js'],
-};
-
-// initialize swagger-jsdoc
-const swaggerSpec = swaggerJSDoc(options);
-
 const port = 3001;
 app.use(express.json());
-app.use('/', routes);
+app.use('/', folderRoutes);
+app.use('/', swaggerDocs);
 // require('./routes')(app);
 
 app.listen(port, () => logger.info(`App listening on port ${port}`));
-
-app.get('/swagger.json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(swaggerSpec);
-});
 
 module.exports = app;
---
+++ server/routes/docs/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const logger = require('../../services/logger');
+
+const router = express.Router();
+
+const swaggerDefinition = {
+  info: {
+    title: 'Notes API',
+    version: '1.0.0',
+    description: 'Simple note taking app',
+  },
+  host: 'localhost:3000',
+  basePath: '/',
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ['./server/routes/folder/*.js'],
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
+router.get('/swagger.json', (req, res) => {
+  logger.info('Swagger docs initialized');
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+
+module.exports = router;
