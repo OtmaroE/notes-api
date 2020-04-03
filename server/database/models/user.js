279028adbbbce21c8c684c5385bfac7e2f736bfb
Jose Gonzalez <otmaro@outlook.com>
2020-04-02 18:38:23 -0600

    Update user model

--- server/database/models/user.js
+++ server/database/models/user.js
@@ -1,16 +1,12 @@
-const Sequelize = require('sequelize');
-
-const int = Sequelize.INTEGER;
-const string = Sequelize.STRING;
-const bool = Sequelize.BOOLEAN;
-
-class User extends Model {}
-
-User.init({
-  id: { type: int, primaryKey: true, autoIncrement: true },
-  name: { type: string },
-  email: { type: string },
-  password: { type: string },
-  isDeleted: { type: bool, field: 'is_deleted' },
-  roleId: 'todo: this should be a foreign key to roles',
-});
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    userName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    isDeleted: DataTypes.BOOLEAN,
+  }, {});
+  User.associate = (models) => {
+    // associations can be defined here
+  };
+  return User;
+};
