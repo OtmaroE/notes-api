68afe28ca5e4f19f48c3a286f6c67f3501879700
Jose Gonzalez <otmaro@outlook.com>
2023-02-18 12:45:58 -0600

    Update user migration

--- server/database/migrations/20200723022331-create-notes.js
+++ server/database/migrations/20200723022331-create-notes.js
@@ -1,43 +1,51 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Notes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      content: {
-        type: Sequelize.STRING,
-      },
-      folder: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: {
-            tableName: 'Folders',
-            schema: 'public',
-          },
-          key: 'id',
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Notes', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    content: {
+      type: Sequelize.STRING,
+    },
+    folderId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: {
+          tableName: 'Folders',
+          schema: 'public',
         },
-        allowNull: false,
+        key: 'id',
       },
-      isDeleted: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
+      allowNull: false,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: {
+          tableName: 'Users',
+          schema: 'public',
+        },
+        key: 'id',
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
+    },
+    isDeleted: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
   down: (queryInterface) => queryInterface.dropTable('Notes'),
 };
